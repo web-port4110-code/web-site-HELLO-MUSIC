b7cdeedb013188ccd8aa09e48721363912f3eb9d
rhythm115 <sansanwasenwa.narinari.1111@gmail.com>
2025-10-01 17:18:36 +0900

    コンサート情報モジュールを作成しました

--- jq/motion.js
+++ jq/motion.js
@@ -1,4 +1,6 @@
 $(function() {
+
+    //heaader-navのアニメーション
     $('.headerBox__button--close a').on('click', function(){
         $('.navigation').toggleClass('open');
         $('.headerBox__button--open').toggleClass('hide');
@@ -7,5 +9,16 @@ $(function() {
     $('.headerBox__button--open a').on('click', function(){
         $('.navigation').toggleClass('open');
         $('.headerBox__button--open').toggleClass('hide');
+    });
+
+    //concertInfoの切り替え機能
+    $('select').on('change', function(){
+        const selectElement = $(this).get(0);
+        const selectValue = selectElement.value;
+        const options = $('.concertInfo__tab div').get();
+
+        $('.active').removeClass('active');
+        options[selectValue - 1].classList.add('active');
+
     });
 })
