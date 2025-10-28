6c53bbd1178e2a641aa1773cabd5b634e9fa6e6c
k-yama-w3dev <k.yama.w3dev2560@gmail.com>
2025-10-28 13:27:48 +0900

    ヘッダナビのリンクを作成しました

--- php_mojure/header.php
+++ php_mojure/header.php
@@ -26,8 +26,8 @@
     </div>
     <nav>
         <ul class="navigation">
-            <li>HOME</li>
-            <li>チケット予約</li>
+            <li><a href="./index.php">HOME</a></li>
+            <li><a href="./form.php">チケット予約</a></li>
             <li><a href="#" data-bs-toggle="modal" data-bs-target="#instrument">楽器紹介</a></li>
         </ul>
     </nav>
